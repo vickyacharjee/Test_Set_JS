9a13a41ec5733b0c9e20764075d4adecdd34bf75
vicky acharjee <vickyacharjeevlg@gmail.com>
2024-01-23 22:41:26 +0530

    23 JAN 23 [Commit for Res.send File]

--- JS_Critical/HTTP_Server/index.js
+++ JS_Critical/HTTP_Server/index.js
@@ -5,12 +5,26 @@ const fs=require('fs');
 const mySever=http.createServer((req,res)=>{
     // console.log('Recieved new request');
     // console.log(req.headers);
+    // console.log(req);
 
-    const log=`${Date.now().toLocaleString()} as Request Recieved \n`;
+    const log=`${Date.now().toString()} as Request Recieved in 8000 and url seek for ${req.url} \n`;
     fs.appendFile('log.txt',log,(error,data)=>{
-        res.end('Hello vicky from port:8003')
+           switch (req.url) {
+            case '/': res.end('This is home for Server')
+                break;
+        
+            case '/about': res.end('This is about Section')
+                break;
+
+             case '/home': res.end('This is HOME Section')
+                break;    
+            
+            default:
+                res.end("<h1>404 Not found</h1>");
+        }
     })
+    //using switch case for the specified URL
 
 });
 
-mySever.listen(8003,()=>console.log('Server Created')) 
+mySever.listen(8000,()=>console.log('Server Created')) 
